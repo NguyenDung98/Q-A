65f8897c5d7d7d7401224b9b7205d2d3e69352ee
HuyenPm <minhhuyenpham98@gmail.com>
2018-10-31 22:02:46 +0700

    add backend to addSession

---
+++ helpers/session.js
@@ -0,0 +1,31 @@
+let db = require('../models');
+
+module.exports = {
+    createSession(req, res) {
+        db.Session.create(req.body)
+            .then(session => {
+                res.json(session)
+            })
+            .catch(error => {
+                res.send(error)
+            })
+    },
+    getAllSession(req, res) {
+        db.Session.find()
+            .then(sessions => {
+                res.json(sessions)
+            })
+            .catch(error => {
+                res.send(error)
+            })
+    },
+    // updateQuestion(req, res) {
+    //     db.Question.findByIdAndUpdate(req.params.questionID, req.body, {new: true})
+    //         .then(updatedQuestion => {
+    //             res.json(updatedQuestion)
+    //         })
+    //         .catch(error => {
+    //             res.sendFile(error)
+    //         })
+    // }
+};
